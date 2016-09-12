35ceda1c78b084bc3b5bf4e3ebb23a6720de896b
Zeke Fast <zekefast@gmail.com>
2016-09-12 19:00:04 +0200

    Add errors' tagging modules: Error, StandardError.

--- lib/com/mash_ape/google_play_store.rb
+++ lib/com/mash_ape/google_play_store.rb
@@ -3,3 +3,7 @@
 # Purpose: namespace
 module ::Com::MashApe::GooglePlayStore
 end
+
+
+require "com/mash_ape/google_play_store/error"
+require "com/mash_ape/google_play_store/standard_error"
---
+++ lib/com/mash_ape/google_play_store/standard_error.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Purpose: mixin
+#
+# @see ::Com::StandardError
+module ::Com::MashApe::GooglePlayStore::StandardError
+  include ::Com::Http::StandardError
+end
